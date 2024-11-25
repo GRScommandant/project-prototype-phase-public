5a3e3d802491d10ef9e1652dcfb78ebd14ba7a24
ali <ali124455@gmail.com>
2024-11-26 00:55:39 +0330

    -a

--- proje/src/components/stepper/index.jsx
+++ proje/src/components/stepper/index.jsx
@@ -7,29 +7,31 @@ import Step3 from "./step3"; // Ensure there are no spaces in the filename
 import Step4 from "./step4"; // Ensure there are no spaces in the filename
 import { useEffect, useState } from "react";
 import auth from "../../lib/auth"
+import { Flex } from '@radix-ui/themes';
 
-function StepContent({ step, register, errors }) {
+function StepContent({ step, use_form}) {
     switch (step) {
         case 1:
-            return <Step1 register={register} errors={errors} />;
+            return <Step1 use_form={use_form}  />;
         case 2:
-            return <Step2 register={register} errors={errors} />;
+            return <Step2 use_form={use_form} />;
         case 3:
-            return <Step3 register={register} errors={errors} />;
+            return <Step3 use_form={use_form} />;
         case 4:
-            return <Step4 register={register} errors={errors} />;
+            return <Step4 use_form={use_form} />;
         default:
             return <div>Unknown step</div>;
     }
 }
 
 export default function Index() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const use_form = useForm({ defaultValues: { birthdate: null } });
+    const { register, formState: { errors }, handleSubmit } = use_form
     const [currentStep, setCurrentStep] = useState(1);
 
     const onSubmit = async (data) => {
         //auth.register (data);
-        console.log("ok")
+        console.log("ok" , JSON.stringify(data , null , 2))
     };
 
     function stepHandler(step) {
@@ -42,11 +44,11 @@ export default function Index() {
     // }, [currentStep]);
 
     return (
-        <section style={{ paddingTop: '80px' }}>
+        <section style={{ paddingTop: '80px', display:"flex", flexDirection:"column" , alignItems:"center"}}>
             <p>Index</p>
             <p>Current Step: {currentStep}</p>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <StepContent step={currentStep} register={register} errors={errors} />
+            <form onSubmit={handleSubmit(onSubmit)} style={{display:"flex",flexDirection:"column" , maxWidth:"60vw" , minWidth:"50vw" , gap:"0.625rem 0" }}>
+                <StepContent step={currentStep} use_form={use_form}/>
                 {currentStep > 1 && (
                     <button type="button" onClick={() => stepHandler(-1)} aria-label="Previous Step">Previous</button>
                 )}
--- proje/src/components/stepper/step1.jsx
+++ proje/src/components/stepper/step1.jsx
@@ -1,10 +1,55 @@
-export default function step1 () {
+import React, { useEffect, useState } from "react";
+//import { useForm } from 'react-hook-form';
+import { Calendar } from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+
+export default function Step1({ use_form }) {
+    const { register, formState: { errors }, setValue, getValues } = use_form
+
+    const [, setUpdate] = useState(false);
+    const [showCalendar, setShowCalendar] = useState(false);
+
+    const birthdateValue = getValues("birthdate");
+
+
+    const handelChangedate = (date) => {
+        setValue("birthdate", (new Date(date)).toLocaleDateString('fa'));
+        setUpdate(update => !update) // Update the birthdate value
+    };
+
+
 
     return (
         <>
-        <input type="text" />
+            <input type="text" placeholder="نام" {...register("firstname", { required: true, maxLength: 40 })} />
+            {errors.firstname && <span>وارد کردن نام الزامیست</span>}
+            <input type="text" placeholder=" نام خانوادگی" {...register("lastname", { required: true, maxLength: 40 })} />
+            {errors.lastname && <span>وارد کردن نام خانوادگی الزامیست</span>}
+            <input type="email" placeholder="ایمیل" {...register("email", { required: true, minLength: 5, maxLength: 80 })} />
+            {errors.email && <span>وارد کردن ایمیل الزامیست</span>}
+            <input type="tel" placeholder="شماره موبایل" {...register("phonenumber", { required: true, minLength: 6, maxLength: 12 })} />
+            {errors.phonenumber && <span>وارد کردن شماره موبایل الزامیست</span>}
+            <label>
+                <input type="radio" value="yes" {...register("radio", { required: true })} />
+                بله
+                <input type="radio" value="no" {...register("radio", { required: true })} />
+                خیر
+                {errors.radio && <span>یکی از گزینه ها را انتخاب کنید</span>}
+            </label>
+            <select {...register("option", { required: true })}>
+                <option value="">انتخاب کنید</option>
+                <option value="دبیرستان دوره اول">دبیرستان دوره اول</option>
+                <option value="دبیرستان دوره دوم">دبیرستان دوره دوم</option>
+                <option value="دبستان">دبستان</option>
+                <option value="پیش دانشگاه">پیش دانشگاه</option>
+            </select>
+            {errors.option && <span>یک مورد را انتخاب کنید</span>}
+            <div>
+                <button type="button" onClick={() => setShowCalendar(p => !p)}>تاریخ تولد</button>
+                <p>{birthdateValue}</p> {/* Render the formatted date */}
+                {showCalendar ? <Calendar calendar={persian} locale={persian_fa} onChange={handelChangedate} /> : null}
+            </div>
         </>
-    )
-    
-    
-    }
+    );
+}
